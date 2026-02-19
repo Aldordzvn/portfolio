f6d2226936cc014623022360e4a01bea6defa75e
Aldordzvn <aldordzvn2001@gmail.com>
2026-02-18 18:53:35 -0700

    Se añade experiencias

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
 import { LandingComponent } from './landing/landing.component';
+import { ExperienciaComponent } from './experiencia/experiencia.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, LandingComponent],
+  imports: [RouterOutlet, HeaderComponent, LandingComponent, ExperienciaComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
